54c0429b4f47ab81bc7a2b00972305312734f8eb
Cedric David <cedric.david@btopenworld.com>
2018-09-02 13:40:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,40 +1,28 @@
-def roll_call_dwarves(array)# code an argument here
-  # Your code here
-  array.each_with_index do |dwarf, index|
-    puts "#{index +1} #{dwarf}"
-
-  end
-
+def roll_call_dwarves(array)
+  array.each_with_index {|dwarf, index| puts "#{index + 1} #{dwarf}"}
 end
 
-def summon_captain_planet(array)# code an argument here
-  # Your code here
-    planets = []
-    array.each do |planet|
-      planets.push("#{planet.capitalize}!")
-
-    end
-    planets
-end
-
-def long_planeteer_calls(array)# code an argument here
-  # Your code here
-  array.all? do |word|
-    if word.length > 4
-      return true
-    end
-
+def summon_captain_planet(array)
+  array.map do |planeteer|
+    "#{planeteer.capitalize}!"
   end
 end
 
-def find_the_cheese(array)# code an argument here
-  # the array below is here to help
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.size > 4
+  end
+end
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each do |cheese|
-    if array.include?(cheese) == !false
-      return cheese
-    else
-      return nil
+  array.find do |item|
+    i = 0
+    while i < 3 do
+      if item == cheese_types[i]
+        return item
+      end
+      i += 1
     end
   end
 end
